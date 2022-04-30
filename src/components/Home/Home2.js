abc8160a761ef90b3008d9303b400bd1c9ced612
Shaik Gafoor <97434962+gafoor8374@users.noreply.github.com>
2022-04-30 21:50:17 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/photo-modified.png";
+import myImg from "../../Assets/photo.png";
 import Tilt from "react-parallax-tilt";
 import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+
 function Home2() {
   return (
     <Container fluid className="home-about-section" id="about">
